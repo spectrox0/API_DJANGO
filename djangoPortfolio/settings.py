ee5e23806c91df772dbea6b7748794b2d305af69
alejandro <alejanvelazco2008@hotmail.com>
2020-10-03 07:07:24 -0400

    cas

--- djangoPortfolio/settings.py
+++ djangoPortfolio/settings.py
@@ -124,6 +124,6 @@ django_heroku.settings(locals())
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
-    'https://alevelazco.com', 'http://localhost:3000,', ]
+    'https://alevelazco.com', 'http://localhost:3000', ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
