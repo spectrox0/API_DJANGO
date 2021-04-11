d729cc28326fcb22d29757578acffd90ba95ca83
alejandro <alejandro.velazco@fidecorp.com>
2021-04-10 22:06:44 -0400

    add serializer image

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_works,get_categories,get_images,csrf
+from .views import get_works,get_categories,get_images, get_work
 
 urlpatterns = [
     path("work/", get_works, name="get-works"),
+    path("work/<pk>", get_work, name="get-work"),
     path("category/", get_categories, name="get-categories"),
     path("image/",get_images, name="get-images"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .serializers import WorkSerializer, CategorySerializer ,ImageSerializer
-from .models import Work, Category, Image
-from django.http import JsonResponse
 from django.middleware.csrf import get_token
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Work, Category, Image
+from .serializers import WorkSerializer, CategorySerializer, ImageSerializer
+
+
 # Create your views here.
 
 
 @api_view(["GET"])
 def get_works(request):
-    works = Work.objects.all()
-    serializer = WorkSerializer(works, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        works = Work.objects.all()
+        serializer = WorkSerializer(works, many=True)
+        return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_work(request, pk):
+    try:
+        work = Work.objects.get(pk=pk)
+    except Work.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == "GET":
+        serializer = WorkSerializer(work)
+        return Response(serializer.data)
+
+
 
 @api_view(["GET"])
 def get_categories(request):
@@ -20,19 +37,20 @@ def get_categories(request):
     serializer = CategorySerializer(categories, many=True)
     return Response(serializer.data)
 
+
 @api_view(["GET"])
 def get_images(request):
     images = Image.objects.all().select_related()
-    
+
     serializer = ImageSerializer(images, many=True)
     print(serializer.data)
     return Response(serializer.data)
-    
 
 
 # todolist/views.py
 def csrf(request):
     return JsonResponse({'csrfToken': get_token(request)})
+
 
 def ping(request):
     return JsonResponse({'result': 'OK'})
